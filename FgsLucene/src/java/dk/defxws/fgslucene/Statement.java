26499c42dd5cc79c546c1b0a2d198f6587d9bb5b
Gert Schmeltz Pedersen <gsp@dtic.dtu.dt>
2008-11-05 15:06:04 +0000

    Added tests. Minor modifications.

--- FgsLucene/src/java/dk/defxws/fgslucene/Statement.java
+++ FgsLucene/src/java/dk/defxws/fgslucene/Statement.java
@@ -110,7 +110,6 @@ public class Statement {
     	} catch (IOException e) {
     		throw new GenericSearchException(e.toString());
     	}
-//    	Hits hits = getHits(query, sortFields);
     	int start = Integer.parseInt(Integer.toString(startRecord));
     	TopDocs hits = getHits(query, start+maxResults-1, sortFields);
     	ScoreDoc[] docs = hits.scoreDocs;
@@ -211,8 +210,7 @@ public class Statement {
 //locale          ::= language['-'country['-'variant]]
 //comparatorClass ::= package-path'.'className['('param['-'param]*')']
 //reverse         ::= 'false' (default) | 'true' | 'reverse'
-  	private TopDocs getHits(Query query, int numHits, String sortFields) throws GenericSearchException {
-//    	Hits hits = null;
+    private TopDocs getHits(Query query, int numHits, String sortFields) throws GenericSearchException {
     	TopDocs hits = null;
     	IndexReader ireader = searcher.getIndexReader();
     	Collection fieldNames = ireader.getFieldNames(IndexReader.FieldOption.ALL);
@@ -222,10 +220,10 @@ public class Statement {
     	SortField[] sortFieldArray = new SortField[st.countTokens()];
     	int i = 0;
     	while (st.hasMoreTokens()) {
-        	SortField sortField = null;
+    		SortField sortField = null;
     		String sortFieldString = st.nextToken().trim();
     		if (sortFieldString.length()==0)
-	            errorExit("getHits sortFields='"+sortFields+"' : empty sortField string");
+    			errorExit("getHits sortFields='"+sortFields+"' : empty sortField string");
     		StringTokenizer stf = new StringTokenizer(sortFieldString, ",");
     		if (!stf.hasMoreTokens())
     			errorExit("getHits sortFields='"+sortFields+"' : empty sortFieldName string in '" + sortFieldString + "'");
@@ -235,178 +233,161 @@ public class Statement {
     		if (!fieldNames.contains(sortFieldName))
     			errorExit("getHits sortFields='"+sortFields+"' : sortFieldName '" + sortFieldName + "' not found as index field name");
     		if (!stf.hasMoreTokens()) {
-            	sortField = new SortField(sortFieldName);
+    			sortField = new SortField(sortFieldName);
     		} else {
     			String sortTypeOrLocaleOrCompString = stf.nextToken().trim();
-        		if (sortTypeOrLocaleOrCompString.length()==0)
-        			errorExit("getHits sortFields='"+sortFields+"' : empty sortType or locale or comparatorClass string in '" + sortFieldString + "'");
-        		if (sortTypeOrLocaleOrCompString.indexOf(".")>=0) {
-        			String compString = sortTypeOrLocaleOrCompString;
-        			String paramString = "";
-        			Object[] params = new Object[] {};
-            		if (sortTypeOrLocaleOrCompString.indexOf("(")>=0) {
-            			int p = compString.indexOf("(");
-            			int q = compString.indexOf(")");
-            			if (p<3 || q<p+1)
-            				errorExit("getHits sortFields='"+sortFields+"' : comparatorClass parameters malformed in '" + compString + "'.");
-            			paramString = compString.substring(p+1, q);
-            			compString = compString.substring(0, p);
-                		StringTokenizer stp = new StringTokenizer(paramString, "-");
-            			params = new Object[stp.countTokens()];
-//            			params[0] = searcher.getIndexReader();
-//            			params[1] = sortFieldName;
-            			int ip = 0;
-                    	while (stp.hasMoreTokens()) {
-                    		params[ip++] = stp.nextToken().trim();
-                    	}
-            		}
-        			SortComparatorSource scs = null;
-//        			try {
-        				Class comparatorClass = null;
-						try {
-							comparatorClass = Class.forName(compString);
-						} catch (ClassNotFoundException e) {
-	        				errorExit("getHits sortFields='"+sortFields+"' : comparatorClass '" + compString + "'"
-    						+ ": class not found:\n"+e.toString());
-						}
-        				Constructor[] constructors = comparatorClass.getConstructors();
-        				StringBuffer errorMessage = new StringBuffer();
-        				for (int j=0; j<constructors.length; j++) {
-        					Constructor cj = constructors[j];
-        					try {
-								scs = (SortComparatorSource) cj.newInstance(params);
-						        if (logger.isDebugEnabled())
-						        	logger.debug("getHits sortFields='"+sortFields+"' : comparatorClass '" 
-						        			+ compString + "'"
-				    						+ ": constructor["+j+"]='"+cj.toGenericString()+"'");
-								break;
-							} catch (IllegalArgumentException e) {
-								errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
-							} catch (InstantiationException e) {
-								errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
-							} catch (IllegalAccessException e) {
-								errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
-							} catch (InvocationTargetException e) {
-								errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
-							}
-        				}
-        				if (scs==null) {
-        					errorExit("getHits sortFields='"+sortFields+"' : comparatorClass '" + compString + "'"
-            						+ ": no constructor applied:\n"+errorMessage.toString());
-        				}
-            		if (!stf.hasMoreTokens()) {
-        				sortField = new SortField(sortFieldName, scs);
-            		} else {
-            			String reverseString = stf.nextToken().trim();
-                		if (reverseString.length()==0)
-                			errorExit("getHits sortFields='"+sortFields+"' : empty reverse string in '" + sortFieldString + "'");
-            			boolean reverse = false;
-            			if ("true".equalsIgnoreCase(reverseString)) reverse = true;
-            			else if ("reverse".equalsIgnoreCase(reverseString)) reverse = true;
-            			else if ("false".equalsIgnoreCase(reverseString)) reverse = false;
-            			else
-            				errorExit("getHits sortFields='"+sortFields+"' : unknown reverse string '" + reverseString + "' in '" + sortFieldString + "'");
-        				sortField = new SortField(sortFieldName, scs, reverse);
-            		}
-        		} else {
-        			String sortTypeOrLocaleString = sortTypeOrLocaleOrCompString;
-        			int sortType = -1;
-        			Locale locale = null;
-        			if ("AUTO".equals(sortTypeOrLocaleString)) sortType = SortField.AUTO;
-        			else if ("DOC".equals(sortTypeOrLocaleString)) sortType = SortField.DOC;
-        			else if ("SCORE".equals(sortTypeOrLocaleString)) sortType = SortField.SCORE;
-        			else if ("INT".equals(sortTypeOrLocaleString)) sortType = SortField.INT;
-        			else if ("FLOAT".equals(sortTypeOrLocaleString)) sortType = SortField.FLOAT;
-        			else if ("STRING".equals(sortTypeOrLocaleString)) sortType = SortField.STRING;
-        			else if (((sortTypeOrLocaleString.substring(0, 1)).compareTo("A") >= 0) && ((sortTypeOrLocaleString.substring(0, 1)).compareTo("Z") <= 0)) {
-        				errorExit("getHits sortFields='"+sortFields+"' : unknown sortType string '" + sortTypeOrLocaleString + "' in '" + sortFieldString + "'");
-        			}
-        			else {
-                		StringTokenizer stfl = new StringTokenizer(sortTypeOrLocaleString, "-");
-                		if (stfl.countTokens()>3)
-                			errorExit("getHits sortFields='"+sortFields+"' : unknown locale string '" + sortTypeOrLocaleString + "' in '" + sortFieldString + "'");
-                		String language = stfl.nextToken().trim();
-                		if (language.length()==0)
-                			errorExit("getHits sortFields='"+sortFields+"' : empty language string in '" + sortFieldString + "'");
-                		if (language.length()>2)
-                			errorExit("getHits sortFields='"+sortFields+"' : unknown language string '" + language + "' in '" + sortFieldString + "'");
-                		if (!stfl.hasMoreTokens()) {
-                        	locale = new Locale(language);
-                		} else {
-                			String country = stfl.nextToken().trim();
-                    		if (country.length()==0)
-                    			errorExit("getHits sortFields='"+sortFields+"' : empty country string in '" + sortFieldString + "'");
-                    		if (country.length()>3)
-                    			errorExit("getHits sortFields='"+sortFields+"' : unknown country string '" + country + "' in '" + sortFieldString + "'");
-                    		if (!stfl.hasMoreTokens()) {
-                            	locale = new Locale(language, country);
-                    		} else {
-                    			String variant = stfl.nextToken().trim();
-                        		if (variant.length()==0)
-                        			errorExit("getHits sortFields='"+sortFields+"' : empty variant string in '" + sortFieldString + "'");
-                            	locale = new Locale(language, country, variant);
-                    		}
-                		}
-        			}
-            		if (!stf.hasMoreTokens()) {
-            			if (sortType >= 0)
-            				sortField = new SortField(sortFieldName, sortType);
-            			else
-            				sortField = new SortField(sortFieldName, locale);
-            		} else {
-            			String reverseString = stf.nextToken().trim();
-                		if (reverseString.length()==0)
-                			errorExit("getHits sortFields='"+sortFields+"' : empty reverse string in '" + sortFieldString + "'");
-            			boolean reverse = false;
-            			if ("true".equalsIgnoreCase(reverseString)) reverse = true;
-            			else if ("reverse".equalsIgnoreCase(reverseString)) reverse = true;
-            			else if ("false".equalsIgnoreCase(reverseString)) reverse = false;
-            			else
-            	            throw new GenericSearchException("getHits sortFields='"+sortFields+"' : unknown reverse string '" + reverseString + "' in '" + sortFieldString + "'");
-            			if (sortType >= 0)
-            				sortField = new SortField(sortFieldName, sortType, reverse);
-            			else
-            				sortField = new SortField(sortFieldName, locale, reverse);
-            		}
-        		}
+    			if (sortTypeOrLocaleOrCompString.length()==0)
+    				errorExit("getHits sortFields='"+sortFields+"' : empty sortType or locale or comparatorClass string in '" + sortFieldString + "'");
+    			if (sortTypeOrLocaleOrCompString.indexOf(".")>=0) {
+    				String compString = sortTypeOrLocaleOrCompString;
+    				String paramString = "";
+    				Object[] params = new Object[] {};
+    				if (sortTypeOrLocaleOrCompString.indexOf("(")>=0) {
+    					int p = compString.indexOf("(");
+    					int q = compString.indexOf(")");
+    					if (p<3 || q<p+1)
+    						errorExit("getHits sortFields='"+sortFields+"' : comparatorClass parameters malformed in '" + compString + "'.");
+    					paramString = compString.substring(p+1, q);
+    					compString = compString.substring(0, p);
+    					StringTokenizer stp = new StringTokenizer(paramString, "-");
+    					params = new Object[stp.countTokens()];
+    					int ip = 0;
+    					while (stp.hasMoreTokens()) {
+    						params[ip++] = stp.nextToken().trim();
+    					}
+    				}
+    				SortComparatorSource scs = null;
+    				Class comparatorClass = null;
+    				try {
+    					comparatorClass = Class.forName(compString);
+    				} catch (ClassNotFoundException e) {
+    					errorExit("getHits sortFields='"+sortFields+"' : comparatorClass '" + compString + "'"
+    							+ ": class not found:\n"+e.toString());
+    				}
+    				Constructor[] constructors = comparatorClass.getConstructors();
+    				StringBuffer errorMessage = new StringBuffer();
+    				for (int j=0; j<constructors.length; j++) {
+    					Constructor cj = constructors[j];
+    					try {
+    						scs = (SortComparatorSource) cj.newInstance(params);
+    						if (logger.isDebugEnabled())
+    							logger.debug("getHits sortFields='"+sortFields+"' : comparatorClass '" 
+    									+ compString + "'"
+    									+ ": constructor["+j+"]='"+cj.toGenericString()+"'");
+    						break;
+    					} catch (IllegalArgumentException e) {
+    						errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
+    					} catch (InstantiationException e) {
+    						errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
+    					} catch (IllegalAccessException e) {
+    						errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
+    					} catch (InvocationTargetException e) {
+    						errorMessage.append("\nconstructor["+j+"]='"+cj.toGenericString()+"'"+"\n"+e.toString()+" ");
+    					}
+    				}
+    				if (scs==null) {
+    					errorExit("getHits sortFields='"+sortFields+"' : comparatorClass '" + compString + "'"
+    							+ ": no constructor applied:\n"+errorMessage.toString());
+    				}
+    				if (!stf.hasMoreTokens()) {
+    					sortField = new SortField(sortFieldName, scs);
+    				} else {
+    					String reverseString = stf.nextToken().trim();
+    					if (reverseString.length()==0)
+    						errorExit("getHits sortFields='"+sortFields+"' : empty reverse string in '" + sortFieldString + "'");
+    					boolean reverse = false;
+    					if ("true".equalsIgnoreCase(reverseString)) reverse = true;
+    					else if ("reverse".equalsIgnoreCase(reverseString)) reverse = true;
+    					else if ("false".equalsIgnoreCase(reverseString)) reverse = false;
+    					else
+    						errorExit("getHits sortFields='"+sortFields+"' : unknown reverse string '" + reverseString + "' in '" + sortFieldString + "'");
+    					sortField = new SortField(sortFieldName, scs, reverse);
+    				}
+    			} else {
+    				String sortTypeOrLocaleString = sortTypeOrLocaleOrCompString;
+    				int sortType = -1;
+    				Locale locale = null;
+    				if ("AUTO".equals(sortTypeOrLocaleString)) sortType = SortField.AUTO;
+    				else if ("DOC".equals(sortTypeOrLocaleString)) sortType = SortField.DOC;
+    				else if ("SCORE".equals(sortTypeOrLocaleString)) sortType = SortField.SCORE;
+    				else if ("INT".equals(sortTypeOrLocaleString)) sortType = SortField.INT;
+    				else if ("FLOAT".equals(sortTypeOrLocaleString)) sortType = SortField.FLOAT;
+    				else if ("STRING".equals(sortTypeOrLocaleString)) sortType = SortField.STRING;
+    				else if (((sortTypeOrLocaleString.substring(0, 1)).compareTo("A") >= 0) && ((sortTypeOrLocaleString.substring(0, 1)).compareTo("Z") <= 0)) {
+    					errorExit("getHits sortFields='"+sortFields+"' : unknown sortType string '" + sortTypeOrLocaleString + "' in '" + sortFieldString + "'");
+    				}
+    				else {
+    					StringTokenizer stfl = new StringTokenizer(sortTypeOrLocaleString, "-");
+    					if (stfl.countTokens()>3)
+    						errorExit("getHits sortFields='"+sortFields+"' : unknown locale string '" + sortTypeOrLocaleString + "' in '" + sortFieldString + "'");
+    					String language = stfl.nextToken().trim();
+    					if (language.length()==0)
+    						errorExit("getHits sortFields='"+sortFields+"' : empty language string in '" + sortFieldString + "'");
+    					if (language.length()>2)
+    						errorExit("getHits sortFields='"+sortFields+"' : unknown language string '" + language + "' in '" + sortFieldString + "'");
+    					if (!stfl.hasMoreTokens()) {
+    						locale = new Locale(language);
+    					} else {
+    						String country = stfl.nextToken().trim();
+    						if (country.length()==0)
+    							errorExit("getHits sortFields='"+sortFields+"' : empty country string in '" + sortFieldString + "'");
+    						if (country.length()>3)
+    							errorExit("getHits sortFields='"+sortFields+"' : unknown country string '" + country + "' in '" + sortFieldString + "'");
+    						if (!stfl.hasMoreTokens()) {
+    							locale = new Locale(language, country);
+    						} else {
+    							String variant = stfl.nextToken().trim();
+    							if (variant.length()==0)
+    								errorExit("getHits sortFields='"+sortFields+"' : empty variant string in '" + sortFieldString + "'");
+    							locale = new Locale(language, country, variant);
+    						}
+    					}
+    				}
+    				if (!stf.hasMoreTokens()) {
+    					if (sortType >= 0)
+    						sortField = new SortField(sortFieldName, sortType);
+    					else
+    						sortField = new SortField(sortFieldName, locale);
+    				} else {
+    					String reverseString = stf.nextToken().trim();
+    					if (reverseString.length()==0)
+    						errorExit("getHits sortFields='"+sortFields+"' : empty reverse string in '" + sortFieldString + "'");
+    					boolean reverse = false;
+    					if ("true".equalsIgnoreCase(reverseString)) reverse = true;
+    					else if ("reverse".equalsIgnoreCase(reverseString)) reverse = true;
+    					else if ("false".equalsIgnoreCase(reverseString)) reverse = false;
+    					else
+    						throw new GenericSearchException("getHits sortFields='"+sortFields+"' : unknown reverse string '" + reverseString + "' in '" + sortFieldString + "'");
+    					if (sortType >= 0)
+    						sortField = new SortField(sortFieldName, sortType, reverse);
+    					else
+    						sortField = new SortField(sortFieldName, locale, reverse);
+    				}
+    			}
     		}
     		sortFieldArray[i++] = sortField;
     	}
     	Sort sort = new Sort(sortFieldArray);
-		TopDocCollector collector = null;
-		if (sortFieldArray.length == 0) {
-			collector = new TopDocCollector(numHits);
-		} else {
-			try {
-				collector = new TopFieldDocCollector( ireader, sort, numHits);
-			} catch (IOException e) {
-				errorExit("getHits TopFieldDocCollector sortFields='"+sortFields+"' : "+e.toString());
-			} catch (RuntimeException e) {
-				errorExit("getHits TopFieldDocCollector RuntimeException sortFields='"+sortFields+"' : "+e.toString());
-			}
-		}
-		try {
-			searcher.search(query, collector);
-		} catch (IOException e) {
-			errorExit("getHits search sortFields='"+sortFields+"' : "+e.toString());
-		} catch (RuntimeException e) {
-			errorExit("getHits search RuntimeException sortFields='"+sortFields+"' : "+e.toString());
-		}
-		hits = collector.topDocs();
-//    	try {
-//			TopDocCollector collector = null;
-//    		if (sortFieldArray.length == 0) {
-//    			collector = new TopDocCollector(numHits);
-//    		} else {
-//    			collector = new TopFieldDocCollector( ireader, sort, numHits);
-//    		}
-//			searcher.search(query, collector);
-//			hits = collector.topDocs();
-//		} catch (IOException e) {
-//			errorExit("getHits IOException sortFields='"+sortFields+"' : "+e.toString());
-//		} catch (RuntimeException e) {
-//			errorExit("getHits RuntimeException sortFields='"+sortFields+"' : "+e.toString());
-//		}
+    	TopDocCollector collector = null;
+    	if (sortFieldArray.length == 0) {
+    		collector = new TopDocCollector(numHits);
+    	} else {
+    		try {
+    			collector = new TopFieldDocCollector( ireader, sort, numHits);
+    		} catch (IOException e) {
+    			errorExit("getHits TopFieldDocCollector sortFields='"+sortFields+"' : "+e.toString());
+    		} catch (RuntimeException e) {
+    			errorExit("getHits TopFieldDocCollector RuntimeException sortFields='"+sortFields+"' : "+e.toString());
+    		}
+    	}
+    	try {
+    		searcher.search(query, collector);
+    	} catch (IOException e) {
+    		errorExit("getHits search sortFields='"+sortFields+"' : "+e.toString());
+    	} catch (RuntimeException e) {
+    		errorExit("getHits search RuntimeException sortFields='"+sortFields+"' : "+e.toString());
+    	}
+    	hits = collector.topDocs();
     	return hits;
     }
 
