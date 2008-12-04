d2e42499e2b2356bbace0f99c6e8506345417be3
Gert Schmeltz Pedersen <gsp@dtic.dtu.dt>
2008-12-04 10:20:00 +0000

    test class compare changed, therefore other result.

--- FedoraGenericSearch/src/test/junit/gsearch/test/testsonlucene/TestSortFields.java
+++ FedoraGenericSearch/src/test/junit/gsearch/test/testsonlucene/TestSortFields.java
@@ -53,8 +53,8 @@ public class TestSortFields
 
     @Test
     public void testGfindObjectsSortCompClassWithParams() throws Exception {
-  	    StringBuffer result = doOp("?operation=gfindObjects&query=image&sortFields=dc.title,dk.defxws.fedoragsearch.test.ComparatorSourceTest(1-9)&restXslt=copyXml");
-  	    assertXpathEvaluatesTo("demo:29", "/resultPage/gfindObjects/objects/object[1]/field[@name='PID']/text()", result.toString());
+  	    StringBuffer result = doOp("?operation=gfindObjects&query=image&sortFields=dc.title,dk.defxws.fedoragsearch.test.ComparatorSourceTest(1-9),reverse&restXslt=copyXml");
+  	    assertXpathEvaluatesTo("demo:11", "/resultPage/gfindObjects/objects/object[1]/field[@name='PID']/text()", result.toString());
     }
 
     @Test
