b3f10df1c1ebf5d46ec0fdc451057334d689f760
Gert Schmeltz Pedersen <gsp@dtic.dtu.dt>
2008-12-08 12:34:24 +0000

    Corrected args

--- FedoraGenericSearch/src/sh/client/runSOAPClient.sh
+++ FedoraGenericSearch/src/sh/client/runSOAPClient.sh
@@ -27,4 +27,4 @@
 LIB=../WEB-INF/lib
 JARS=$LIB/fedora-client-3.1.jar:$LIB/log4j-1.2.15.jar:$LIB/activation-1.1.1.jar:$LIB/axis.jar:$LIB/commons-discovery.jar:$LIB/commons-logging.jar:$LIB/jaxrpc-api-1.1.jar:$LIB/mail.jar:$LIB/saaj-api-1.3.jar:$LIB/lucene-core-2.4.0.jar:$LIB/wsdl4j-1.5.1.jar:$LIB/PDFBox-0.7.2.jar:$LIB/xml-apis.jar
 USER=
-java -Xms64m -Xmx96m -Dfedoragsearch.fgsUserName=$fgsUserName -Dfedoragsearch.fgsPassword=$fgsPassword -cp ../WEB-INF/classes:$JARS dk.defxws.fedoragsearch.client.SOAPClient "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+java -Xms64m -Xmx96m -Dfedoragsearch.fgsUserName=$fgsUserName -Dfedoragsearch.fgsPassword=$fgsPassword -cp ../WEB-INF/classes:$JARS dk.defxws.fedoragsearch.client.SOAPClient $1 $2 $3 $4 $5 $6 $7 $8 $9
