3ae4afbba33fbf6f74fbe341751ea70b31d0c01b
Gert Schmeltz Pedersen <gspe@dtic.dtu.dk>
2012-05-16 13:09:22 +0200

    FCREPO-1076 UN_TOKENIZED fields break truncated search

---
+++ FedoraGenericSearch/src/test/junit/gsearch/test/fgs242_1076/AllTests.java
@@ -0,0 +1,31 @@
+//$Id:  $
+package gsearch.test.fgs242_1076;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+/**
+ * Test of GSearch 2.4.2
+ * 
+ * assuming 
+ * - all Fedora demo objects are in the repository referenced in
+ *   configTestOnLucene/repository/FgsRepos/repository.properties
+ * 
+ * the test suite will
+ * - set configTestOnLucene as current config,
+ * - run tests concerning GSearch 2.4.2 
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {
+	gsearch.test.fgs242_1076.TestFgs242_1076.class
+	} )
+public class AllTests {
+
+    // Supports legacy tests runners
+    public static junit.framework.Test suite() throws Exception {
+        junit.framework.TestSuite suite =
+                new junit.framework.TestSuite(AllTests.class.getName());
+        suite.addTest(gsearch.test.fgs242_1076.TestFgs242_1076.suite());
+        return suite;
+    }
+}
---
+++ FedoraGenericSearch/src/test/junit/gsearch/test/fgs242_1076/TestConfigSetup.java
@@ -0,0 +1,35 @@
+//$Id:  $
+package gsearch.test.fgs242_1076;
+
+import junit.framework.Test;
+
+/**
+ * Test of GSearch 2.4.2
+ * 
+ * assuming 
+ * - all Fedora demo objects are in the repository referenced in
+ *   configTestOnLucene/repository/FgsRepos/repository.properties
+ * 
+ * the test suite will
+ * - set configTestOnLucene as current config,
+ * - run tests concerning GSearch 2.4.2 
+ */
+public class TestConfigSetup
+        extends junit.extensions.TestSetup {
+
+    public TestConfigSetup(Test test) {
+        super(test);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        System.out.println("setUp TestFgs242_1076");
+        System.setProperty("fedoragsearch.fgsUserName", "fgsTester");
+        System.setProperty("fedoragsearch.fgsPassword", "fgsTesterPassword");
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        System.out.println("tearDown TestFgs242_1076");
+    }
+}
